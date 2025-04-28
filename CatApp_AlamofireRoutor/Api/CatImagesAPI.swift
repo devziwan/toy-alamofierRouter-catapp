0b34fee6b24630e6ac3174de1338cf95ce44e14f
devziwan <yoojiwan@icloud.com>
2025-04-28 12:34:34 +0900

    Alamofier Router 적용하기 완료git

--- CatApp_AlamofireRoutor/Api/CatImagesAPI.swift
+++ CatApp_AlamofireRoutor/Api/CatImagesAPI.swift
@@ -13,36 +13,36 @@ import Alamofire
 
 enum CatImagesAPI {
     
-//    static let endPoint: String = "https://api.thecatapi.com/"
-   
+    //    static let endPoint: String = "https://api.thecatapi.com/"
+    
     // MARK: GET
     /// 서버에서 고양이 이미지을 가져옵니다.
     /// - Parameter imageLimit: 고양이 이미지 최대 요청 수를 제한합니다.
     /// - Parameter completion: 서버에서 받은 응답 및 에러
     static func fetchCatImage(imageLimit: Int, completion: @escaping (Result<[CatImageResponse], Error>) -> Void) {
         
-//        let urlString: String = endPoint + "v1/images/search" + "?limit=\(imageLimit)"
-//        print(#file, #function, #line, "routorURL: \(urlString)")
-//        AF.request(urlString, method: .get, parameters: nil, headers: [
-//            "Content-Type": "application",
-//            "x-api-key": "live_xLST3RHbCI8ZlLXfi7PG8uwm9GjmFsiqiAz4yrtWVGtCXeB7wZELTOZEAfnfF3Jf"
-//        ])
+        //        let urlString: String = endPoint + "v1/images/search" + "?limit=\(imageLimit)"
+        //        print(#file, #function, #line, "routorURL: \(urlString)")
+        //        AF.request(urlString, method: .get, parameters: nil, headers: [
+        //            "Content-Type": "application",
+        //            "x-api-key": "live_xLST3RHbCI8ZlLXfi7PG8uwm9GjmFsiqiAz4yrtWVGtCXeB7wZELTOZEAfnfF3Jf"
+        //        ])
         
         // TODO: 라우터 패턴 적용하기
         AF.request(CatRouter.fetch(imageLimit: imageLimit))
-        .responseDecodable(of: [CatImageResponse].self) { response in
-            switch response.result {
-            case .success(let catImages):
-                print(#file, #function, #line, "-✅ JSON 디코드 성공")
-                completion(Result.success(catImages))
-            case .failure(let error):
-                print(#file, #function, #line, "-💣 JSON 디코드 실패")
-                completion(Result.failure(error))
+            .responseDecodable(of: [CatImageResponse].self) { response in
+                switch response.result {
+                case .success(let catImages):
+                    print(#file, #function, #line, "-✅ JSON 디코드 성공")
+                    completion(Result.success(catImages))
+                case .failure(let error):
+                    print(#file, #function, #line, "-💣 JSON 디코드 실패")
+                    completion(Result.failure(error))
+                }
             }
-        }
         
     }
-
+    
     // MARK: POST
     /// 고양이 이미지를 서버에 업로드합니다
     /// - Parameters:
@@ -51,18 +51,18 @@ enum CatImagesAPI {
     static func uploadCatImage(selected fileData: Data, completion: @escaping (Result<UploadCatImageResponse, Error>) -> Void) {
         print(#file, #function, #line, "- 고양이 이미지 업로드 요청")
         
-//        let urlString: String = endPoint + "v1/images/upload"
-//        let boundary: String = UUID().uuidString
-//        
-//        let headers: HTTPHeaders = [
-//            "x-api-key": "live_xLST3RHbCI8ZlLXfi7PG8uwm9GjmFsiqiAz4yrtWVGtCXeB7wZELTOZEAfnfF3Jf",
-//            "Content-Type": "multipart/form-data; boundary=\(boundary)"
-//        ]
-//        
-//        AF.upload(multipartFormData: { multipartFormData in
-//            multipartFormData.append(fileData, withName: "file", fileName: "cat.jpeg", mimeType: "image/jpeg" )
-//            
-//        }, to: urlString, method: .post, headers: headers)
+        //        let urlString: String = endPoint + "v1/images/upload"
+        //        let boundary: String = UUID().uuidString
+        //
+        //        let headers: HTTPHeaders = [
+        //            "x-api-key": "live_xLST3RHbCI8ZlLXfi7PG8uwm9GjmFsiqiAz4yrtWVGtCXeB7wZELTOZEAfnfF3Jf",
+        //            "Content-Type": "multipart/form-data; boundary=\(boundary)"
+        //        ]
+        //
+        //        AF.upload(multipartFormData: { multipartFormData in
+        //            multipartFormData.append(fileData, withName: "file", fileName: "cat.jpeg", mimeType: "image/jpeg" )
+        //
+        //        }, to: urlString, method: .post, headers: headers)
         
         AF.upload(multipartFormData: { multipartFormData in
             multipartFormData.append(fileData, withName: "file", fileName: "cat.jpeg", mimeType: "image/jpeg" )
@@ -72,7 +72,7 @@ enum CatImagesAPI {
             
             // 고양이 업로드 응답을 받았으면 ToastActivity가 사라진다.
             NotificationCenter.default.post(name: .uploadSuccessToastEvenet, object: nil, userInfo: nil)
-        
+            
             switch response.result {
             case .success(let result):
                 print(#file, #function, #line, "-✅ JSON 디코드 성공")
@@ -81,7 +81,7 @@ enum CatImagesAPI {
             case .failure(let error):
                 completion(Result.failure(error))
             }
-            
+    
         })
         
     }
@@ -93,14 +93,14 @@ enum CatImagesAPI {
     ///   - completion: 서버에서 받은 응답 및 에러
     static func fetchUploadCatImage(imageLimit: Int, completion: @escaping (Result<[UploadCatImage], Error>) -> Void) {
         print(#file, #function, #line, "- 업로드 이미지 조회 요청 ")
-//        let urlString: String = endPoint + "v1/images/?limit=\(imageLimit)&page=0&order=DESC"
-//        
-//        let headers: HTTPHeaders = [
-//            "Content-Type": "application/json",
-//            "x-api-key": "live_xLST3RHbCI8ZlLXfi7PG8uwm9GjmFsiqiAz4yrtWVGtCXeB7wZELTOZEAfnfF3Jf"
-//        ]
+        //        let urlString: String = endPoint + "v1/images/?limit=\(imageLimit)&page=0&order=DESC"
+        //
+        //        let headers: HTTPHeaders = [
+        //            "Content-Type": "application/json",
+        //            "x-api-key": "live_xLST3RHbCI8ZlLXfi7PG8uwm9GjmFsiqiAz4yrtWVGtCXeB7wZELTOZEAfnfF3Jf"
+        //        ]
         
-//        AF.request(urlString, method: .get, headers: headers)
+        //        AF.request(urlString, method: .get, headers: headers)
         AF.request(CatRouter.fetchUpload(imageLimit: imageLimit))
             .responseDecodable(of: [UploadCatImage].self, completionHandler: { response in
                 
@@ -114,7 +114,6 @@ enum CatImagesAPI {
                 }
                 
             })
-     
     }
     
     // MARK: GET
@@ -125,15 +124,15 @@ enum CatImagesAPI {
     static func deleteUploadCatImage(imageID: String, completion: @escaping (Result<DeleteUploadResponse, Error>) -> Void) {
         print(#file, #function, #line, "- 업로드 삭제 요청")
         
-//        let urlString: String = endPoint + "v1" + "/images/" + "\(imageID)"
-//        print(#file, #function, #line, "\(urlString)")
-//        
-//        let headers: HTTPHeaders = [
-//            "Content-Type": "application/json",
-//            "x-api-key": "live_xLST3RHbCI8ZlLXfi7PG8uwm9GjmFsiqiAz4yrtWVGtCXeB7wZELTOZEAfnfF3Jf"
-//        ]
-//        
-//        AF.request(urlString, method: .delete, parameters: nil, headers: headers)
+        //        let urlString: String = endPoint + "v1" + "/images/" + "\(imageID)"
+        //        print(#file, #function, #line, "\(urlString)")
+        //
+        //        let headers: HTTPHeaders = [
+        //            "Content-Type": "application/json",
+        //            "x-api-key": "live_xLST3RHbCI8ZlLXfi7PG8uwm9GjmFsiqiAz4yrtWVGtCXeB7wZELTOZEAfnfF3Jf"
+        //        ]
+        //
+        //        AF.request(urlString, method: .delete, parameters: nil, headers: headers)
         AF.request(CatRouter.delete(imageID: imageID))
             .validate(statusCode: 200..<300)
             .responseDecodable(of: DeleteUploadResponse.self, completionHandler: { response in
@@ -146,7 +145,6 @@ enum CatImagesAPI {
                     print(#file, #function, #line, "- 💣JSON 디코드 실패.")
                 }
             })
-        
     }
     
     // MARK: - 즐겨찾기 API
@@ -159,18 +157,18 @@ enum CatImagesAPI {
     static func createFavoriteCatImage(imageID: String, completion: @escaping (Result<CreateFavoriteResponse, Error>) -> Void) {
         print(#file, #function, #line, "- 즐겨찾기 요청 했습니다.")
         
-//        let urlString: String = endPoint + "v1" + "/favourites"
-//    
-//        let headers: HTTPHeaders = [
-//            "Content-Type": "application/json",
-//            "x-api-key": "live_xLST3RHbCI8ZlLXfi7PG8uwm9GjmFsiqiAz4yrtWVGtCXeB7wZELTOZEAfnfF3Jf"
-//        ]
-//        
-//        let body: [String: Any] = [
-//            "image_id": imageID,
-//        ]
-//        
-//        AF.request(urlString, method: .post, parameters: body, encoding: JSONEncoding.default ,headers: headers)
+        //        let urlString: String = endPoint + "v1" + "/favourites"
+        //
+        //        let headers: HTTPHeaders = [
+        //            "Content-Type": "application/json",
+        //            "x-api-key": "live_xLST3RHbCI8ZlLXfi7PG8uwm9GjmFsiqiAz4yrtWVGtCXeB7wZELTOZEAfnfF3Jf"
+        //        ]
+        //
+        //        let body: [String: Any] = [
+        //            "image_id": imageID,
+        //        ]
+        //
+        //        AF.request(urlString, method: .post, parameters: body, encoding: JSONEncoding.default ,headers: headers)
         AF.request(CatFavoriteRouter.create(imageID: imageID))
             .responseDecodable(of: CreateFavoriteResponse.self, completionHandler: { response in
                 switch response.result {
@@ -189,15 +187,15 @@ enum CatImagesAPI {
     /// 즐겨찾기 등록 했던 고양이 이미지를 가져옵니다.
     /// - Parameter completion: 서버에서 받은 응답 및 에러
     static func fetchFavoritesCatImages(completion: @escaping (Result<[AllFavoriteResponse], Error>) -> Void) {
-         
-//         let urlString: String = endPoint + "v1/favourites"
-//        
-//        let headers: HTTPHeaders = [
-//            "Content-Type": "application/json",
-//            "x-api-key": "live_xLST3RHbCI8ZlLXfi7PG8uwm9GjmFsiqiAz4yrtWVGtCXeB7wZELTOZEAfnfF3Jf"
-//        ]
-//         
-//        AF.request(urlString, method: .get, parameters: nil, headers: headers)
+        
+        //         let urlString: String = endPoint + "v1/favourites"
+        //
+        //        let headers: HTTPHeaders = [
+        //            "Content-Type": "application/json",
+        //            "x-api-key": "live_xLST3RHbCI8ZlLXfi7PG8uwm9GjmFsiqiAz4yrtWVGtCXeB7wZELTOZEAfnfF3Jf"
+        //        ]
+        //
+        //        AF.request(urlString, method: .get, parameters: nil, headers: headers)
         AF.request(CatFavoriteRouter.fatch)
             .responseDecodable(of: [AllFavoriteResponse].self, completionHandler: { response in
                 switch response.result {
@@ -210,10 +208,10 @@ enum CatImagesAPI {
                     completion(Result.failure(error))
                 }
             })
-
-         
-     }
-
+        
+        
+    }
+    
     
     // MARK: DELETE
     /// 즐겨찾기 등록 했던 고양이 이미지를 삭제합니다.
@@ -223,28 +221,28 @@ enum CatImagesAPI {
     static func deleteFavoriteCatImage(imageID: Int, completion: @escaping (Result<DeleteFavoriteResponse, Error>) -> Void) {
         print(#file, #function, #line, "- 즐겨찾기 삭제 요청 했습니다.")
         
-//        let urlString: String = endPoint + "v1" + "/favourites/" + "\(imageID)"
-//        print(#file, #function, #line, "\(urlString)")
-//        
-//        
-//        let headers: HTTPHeaders = [
-//            "Content-Type": "application/json",
-//            "x-api-key": "live_xLST3RHbCI8ZlLXfi7PG8uwm9GjmFsiqiAz4yrtWVGtCXeB7wZELTOZEAfnfF3Jf"
-//        ]
+        //        let urlString: String = endPoint + "v1" + "/favourites/" + "\(imageID)"
+        //        print(#file, #function, #line, "\(urlString)")
+        //
+        //
+        //        let headers: HTTPHeaders = [
+        //            "Content-Type": "application/json",
+        //            "x-api-key": "live_xLST3RHbCI8ZlLXfi7PG8uwm9GjmFsiqiAz4yrtWVGtCXeB7wZELTOZEAfnfF3Jf"
+        //        ]
         
-//       AF.request(urlString, method: .delete, parameters: nil, headers: headers)
+        //       AF.request(urlString, method: .delete, parameters: nil, headers: headers)
         AF.request(CatFavoriteRouter.delete(imageID: imageID))
-           .responseDecodable(of: DeleteFavoriteResponse.self, completionHandler: { response in
-               switch response.result {
-                   
-               case .success(let result):
-                   print(#file, #function, #line, "- ✅ JSON 디코드 성공 ")
-                   completion(Result.success(result))
-               case .failure(let error):
-                   print(#file, #function, #line, "- 💣JSON 디코드 실패.")
-                   completion(Result.failure(error))
-               }
-           })
+            .responseDecodable(of: DeleteFavoriteResponse.self, completionHandler: { response in
+                switch response.result {
+                    
+                case .success(let result):
+                    print(#file, #function, #line, "- ✅ JSON 디코드 성공 ")
+                    completion(Result.success(result))
+                case .failure(let error):
+                    print(#file, #function, #line, "- 💣JSON 디코드 실패.")
+                    completion(Result.failure(error))
+                }
+            })
     }
     
     
